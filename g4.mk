48cd7787e189a3b3a5af1063a753ed5573d5079b
Arne Coucheron <arco68@gmail.com>
2019-10-09 00:38:18 +0200

    g4-common: gps: Use gps.conf with Lineage's changes
    Change-Id: I7f307e04daa88cdeb6c45d3dfbb2ed0715d91610

--- g4.mk
+++ g4.mk
@@ -130,11 +130,11 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
     android.hardware.gnss@1.0-service \
     gps.msm8992 \
-    quipc.conf
+    quipc.conf \
+    gps.conf
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
     $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
     $(LOCAL_PATH)/gps/etc/lowi.conf:system/etc/lowi.conf \
