ba2bd937364b9772f3c3c2ce0f3f0f1d1f662465
aoleary <seanm187@gmail.com>
2020-01-21 10:54:50 +0000

    delete libmmjpeg_interface for now to get camera to build

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -75,7 +75,7 @@ LOCAL_CFLAGS += -DOPTIMIZE_BUF_COUNT
 endif
 
 LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl libsync
-LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
+LOCAL_SHARED_LIBRARIES += libmmcamera_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libstagefrighthw
 
 LOCAL_MODULE_RELATIVE_PATH := hw
