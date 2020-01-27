LineageOS/android_device_xiaomi_libra

CameraWrapper: close native handles
Android camera framework expects camera HAL to
release native handles in case of video buffer.

Old camera HALs don't release native handles internally,
causing video recording to freeze after ~30s.

This workaround is the equivalent of
https://review.lineageos.org/#/c/165102/

Change-Id: Ie9e9abdede3dccada9e5002725a157f4960643e9

modules/camera-wrapper/CameraWrapper.cpp
@@ -33,6 +33,7 @@
 #include <hardware/camera.h>
 #include <camera/Camera.h>
 #include <camera/CameraParameters.h>
+#include <media/hardware/HardwareAPI.h> // For VideoNativeHandleMetadata
 
 using namespace android;
 
@@ -292,10 +293,16 @@ static void camera_release_recording_frame(struct camera_device *device,
     if (!device)
         return;
 
+    VideoNativeHandleMetadata* md = (VideoNativeHandleMetadata*) data;
+    native_handle_t* nh = md->pHandle;
+
     ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
             (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
 
     VENDOR_CALL(device, release_recording_frame, opaque);
+
+    native_handle_close(nh);
+    native_handle_delete(nh);
 }
 
 static int camera_auto_focus(struct camera_device *device)

LG-G4-POSP/device_lge_g4-common

camera-wrapper/CameraWrapper.cpp
@@ -25,13 +25,14 @@
 #define LOG_TAG "CameraWrapper"
 #include <cutils/log.h>
 #include <cutils/properties.h>
-
+#include <cutils/native_handle.h>
 #include <utils/threads.h>
 #include <utils/String8.h>
 #include <hardware/hardware.h>
 #include <hardware/camera.h>
 #include <camera/Camera.h>
 #include <camera/CameraParameters.h>
+#include <media/hardware/HardwareAPI.h> // For VideoNativeHandleMetadata
 
 #define BACK_CAMERA     0
 #define FRONT_CAMERA    1
@@ -337,10 +338,16 @@ static void camera_release_recording_frame(struct camera_device *device,
     if (!device)
         return;
 
+    VideoNativeHandleMetadata* md = (VideoNativeHandleMetadata*) opaque;
+    native_handle_t* nh = md->pHandle;
+
     ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
             (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
 
     VENDOR_CALL(device, release_recording_frame, opaque);
+
+    native_handle_close(nh);
+    native_handle_delete(nh);
 }
 
 static int camera_auto_focus(struct camera_device *device)