355afae7d06fe05714f0b55516bac5d3ba5499c2
aoleary <seanm187@gmail.com>
2020-01-29 12:54:08 +0000

    g4-common: Modify Clark camera/Android.mk to build on the LG G4

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,4 +1,3 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-camera))
 MM_V4L2_DRIVER_LIST += msm8960
 MM_V4L2_DRIVER_LIST += msm8974
 MM_V4L2_DRIVER_LIST += msm8916
@@ -12,6 +11,9 @@ MM_V4L2_DRIVER_LIST += msm8994
 MM_V4L2_DRIVER_LIST += msm8084
 MM_V4L2_DRIVER_LIST += msm8992
 
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
 ifneq (,$(filter $(MM_V4L2_DRIVER_LIST),$(TARGET_BOARD_PLATFORM)))
   ifneq ($(strip $(USE_CAMERA_STUB)),true)
     ifneq ($(BUILD_TINY_ANDROID),true)
@@ -19,4 +21,10 @@ ifneq (,$(filter $(MM_V4L2_DRIVER_LIST),$(TARGET_BOARD_PLATFORM)))
     endif
   endif
 endif
-endif
+
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
+LOCAL_32_BIT_ONLY := true
+include $(BUILD_SHARED_LIBRARY)
